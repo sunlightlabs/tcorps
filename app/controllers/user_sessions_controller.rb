a00afb039a110420bfcf9a4f0aae12bc63a41963
klondike <kprojection@gmail.com>
2009-04-20 15:44:16 -0400

    #7 - Added ClickPass login services (still missing registration services)

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -15,12 +15,52 @@ class UserSessionsController < ApplicationController
         render :action => :new
       end
     end
+    
+    # Would reach here if the OpenID service can't be discovered
+    flash[:failure] = "Couldn't find OpenID server."
+    redirect_to login_path
   end
   
   def destroy
     current_session.destroy
     flash[:success] = 'You have been logged out.'
     redirect_to root_path
+  end
+  
+  # clickpass
+  
+  def begin_openid_login
+    consumer = OpenID::Consumer.new session, OpenIdAuthentication::DbStore.new
+    openid_url = OpenIdAuthentication.normalize_identifier params[:openid_url]
+    
+    request = consumer.begin openid_url
+    redirect_to request.redirect_url(root_url, complete_openid_login_url)
+  rescue OpenID::DiscoveryFailure
+    # This code should never get reached (invalid OpenID server should have been detected on #create)
+    flash[:failure] = "Couldn't find OpenID server."
+    redirect_to login_path
+  end
+  
+  def complete_openid_login
+    consumer = OpenID::Consumer.new session, OpenIdAuthentication::DbStore.new
+    openid_params = params.reject {|k, v| ['action', 'controller'].include? k}
+    response = consumer.complete openid_params, request.url
+    
+    if response.status == OpenID::Consumer::SUCCESS
+      if @user = User.find_by_openid_identifier(response.identity_url)
+        UserSession.create @user
+        flash[:success] = 'You have been successfully logged in.'
+        redirect_to root_path
+      else
+        # TODO: redirect to Clickpass register/merge form
+        flash[:failure] = "OpenID not known to our system. Register the normal way."
+        redirect_to register_path
+      end
+    else
+      # Not known why this would happen.
+      flash[:failure] = "Invalid OpenID."
+      redirect_to login_path
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ ActionController::Routing::Routes.draw do |map|
   map.register '/register', :controller => 'users', :action => 'new'
   map.login '/login', :controller => 'user_sessions', :action => 'new'
   map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
+  
+  # clickpass
+  map.begin_openid_login '/clickpass/begin_openid_login', :controller => 'user_sessions', :action => 'begin_openid_login'
+  map.complete_openid_login '/clickpass/complete_openid_login', :controller => 'user_sessions', :action => 'complete_openid_login'
 
   map.root :controller => 'campaigns', :action => 'index'
 end
