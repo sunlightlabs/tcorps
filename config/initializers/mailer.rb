121c9fffa8fdf82f52be12fbeb4b3bc74a7a7a3a
klondike <kprojection@gmail.com>
2009-05-15 13:32:02 -0400

    Raise mailer errors even in dev

--- config/initializers/mailer.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sunlightlabs.com',
-  :port => 25,
-  :user_name => 'smtpclient',
-  :password => 'mryogato',
-  :authentication => :plain,
-  :domain => 'sunlightlabs.com'
-}
