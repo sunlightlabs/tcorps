e02d6c5407770e7d984ed3aa3eda781b2d8758d4
klondike <kprojection@gmail.com>
2009-05-15 15:56:35 -0400

    Updated email address

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -1,8 +1,8 @@
 class ContactMailer < ActionMailer::Base
   
   def contact_form(name, email, message)
-    recipients 'tcorps@sunlightlabs.com'
-    from 'tcorps@sunlightlabs.com'
+    recipients 'tcorps@sunlightfoundation.com'
+    from 'tcorps@sunlightfoundation.com'
     subject "Contact form submission from #{name}"
     body :name => name, :email => email, :message => message
   end
