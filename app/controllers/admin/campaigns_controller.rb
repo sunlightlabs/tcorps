f1627524f5dff5bda67e045146f7427f18acd053
klondike <kprojection@gmail.com>
2009-05-29 16:14:40 -0400

    Added #edit and #update actions, beginning to integrate edit form

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -4,8 +4,31 @@ class Admin::CampaignsController < ApplicationController
   skip_before_filter :load_sidebar
   before_filter :require_login
   before_filter :require_manager
+  
+  before_filter :load_campaign, :only => [:edit, :update]
 
   def index
     @campaigns = current_user.campaigns
   end
+  
+  def edit
+  end
+  
+  def update
+    if @campaign.update_attributes params[:campaign]
+      flash[:success] = 'Your campaign has been updated.'
+      redirect_to admin_campaigns_path
+    else
+      render :action => :edit
+    end
+  end
+  
+  protected
+  
+  def load_campaign
+    unless @campaign = Campaign.find_by_id(params[:id]) and @campaign.creator == current_user
+      redirect_to root_path and return false
+    end
+  end
+  
 end
--- test/functional/admin/campaigns_controller_test.rb
+++ test/functional/admin/campaigns_controller_test.rb
@@ -36,4 +36,98 @@ class Admin::CampaignsControllerTest < ActionController::TestCase
     assert_redirected_to root_path
   end
   
+  test '#edit loads a campaign for editing' do
+    user = Factory :manager
+    campaign = Factory :campaign, :creator => user
+    
+    login user
+    get :edit, :id => campaign.id
+    assert_response :success
+    assert_template 'edit'
+    
+    assert_equal campaign, assigns(:campaign)
+  end
+  
+  test '#edit will not load campaigns owned by others' do
+    user = Factory :manager
+    campaign = Factory :campaign
+    
+    login user
+    get :edit, :id => campaign.id
+    assert_redirected_to root_path
+  end
+  
+  test '#edit requires login' do    
+    get :edit, :id => Factory(:campaign)
+    assert_redirected_to root_path
+  end
+  
+  test '#edit requires login as at least a manager' do
+    user = Factory :user
+    campaign = Factory :campaign, :creator => user
+    
+    login user
+    get :edit, :id => campaign
+    assert_redirected_to root_path
+  end
+  
+  test '#update will update a campaign' do
+    user = Factory :manager
+    campaign = Factory :campaign, :creator => user
+    new_name = campaign.name.succ
+    
+    login user
+    put :update, :id => campaign.id, :campaign => {:name => new_name}
+    assert_redirected_to admin_campaigns_path
+    assert_not_nil flash[:success]
+    assert_equal new_name, campaign.reload.name
+  end
+  
+  test '#update renders with errors if the campaign is invalid' do
+    user = Factory :manager
+    campaign = Factory :campaign, :creator => user
+    
+    login user
+    put :update, :id => campaign.id, :campaign => {:name => ''}
+    assert_response :success
+    assert assigns(:campaign).errors.any?
+    
+    assert_not_equal '', campaign.reload.name
+  end
+  
+  test '#update will not update campaigns owned by others' do
+    user = Factory :manager
+    campaign = Factory :campaign
+    new_name = campaign.name.succ
+    
+    login user
+    put :update, :id => campaign.id, :campaign => {:name => new_name}
+    assert_redirected_to root_path
+    assert_nil flash[:success]
+    assert_not_equal new_name, campaign.reload.name
+  end
+  
+  test '#update requires login' do
+    user = Factory :manager
+    campaign = Factory :campaign, :creator => user
+    new_name = campaign.name.succ
+    
+    put :update, :id => campaign.id, :campaign => {:name => new_name}
+    assert_redirected_to root_path
+    assert_nil flash[:success]
+    assert_not_equal new_name, campaign.reload.name
+  end
+  
+  test '#update requires login as at least a manager' do
+    user = Factory :user
+    campaign = Factory :campaign, :creator => user
+    new_name = campaign.name.succ
+    
+    login user
+    put :update, :id => campaign.id, :campaign => {:name => new_name}
+    assert_redirected_to root_path
+    assert_nil flash[:success]
+    assert_not_equal new_name, campaign.reload.name
+  end
+  
 end
