2d7c8da23a168eaff88fad6da1cd147cdeaadb93
klondike <kprojection@gmail.com>
2009-06-15 14:44:41 -0400

    Simplified JS includes

--- public/javascripts/tcorps.js
+++ public/javascripts/tcorps.js
@@ -68,3 +68,16 @@ $(document).ready(function(){
   });
   
 };
+
+function setToNow(object, field) {
+  var base = object + "_" + field;
+  var now = new Date();
+  var minutes = now.getMinutes();
+  if (minutes < 10) minutes = "0" + minutes;
+  
+  $("#" + base + "_1i").val(now.getYear() + 1900);
+  $("#" + base + "_2i").val(now.getMonth() + 1);
+  $("#" + base + "_3i").val(now.getDay());
+  $("#" + base + "_4i").val(now.getHours());
+  $("#" + base + "_5i").val(minutes);
+}
