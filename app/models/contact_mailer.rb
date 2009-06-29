1d3698277436c867f8fbee5a66e5d07cdcdd3d74
klondike <kprojection@gmail.com>
2009-06-29 18:31:17 -0400

    Updated email address so that the email is actually sent

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ActionMailer::Base
   
   def contact_form(name, email, message)
     recipients 'tcorps@sunlightfoundation.com'
-    from 'tcorps@sunlightfoundation.com'
+    from 'tcorps-contact@sunlightfoundation.com'
     subject "[T-Corps] Contact form submission from #{name}"
     body :name => name, :email => email, :message => message
   end
