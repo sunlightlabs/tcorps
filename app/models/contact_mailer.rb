a69a38ccfb43f660dfb7f3e63c6c0767f29b360a
klondike <kprojection@gmail.com>
2009-06-29 18:03:58 -0400

    Updated subject line for contact emails

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
   def contact_form(name, email, message)
     recipients 'tcorps@sunlightfoundation.com'
     from 'tcorps@sunlightfoundation.com'
-    subject "Contact form submission from #{name}"
+    subject "[T-Corps] Contact form submission from #{name}"
     body :name => name, :email => email, :message => message
   end
 
