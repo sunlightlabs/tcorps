4defd3726ef00f3d088000c5a3e79c1eb6a36b0e
klondike <kprojection@gmail.com>
2009-07-10 17:10:14 -0400

    Updated copy to make Transparency Corps two words

--- app/models/campaign_mailer.rb
+++ app/models/campaign_mailer.rb
@@ -2,8 +2,8 @@ class CampaignMailer < ActionMailer::Base
   
   def new_campaign(campaign, user)
     recipients user.email
-    from 'TransparencyCorps <tcorps@sunlightfoundation.com>'
-    subject "A new campaign has appeared on TransparencyCorps"
+    from 'Transparency Corps <tcorps@sunlightfoundation.com>'
+    subject "A new campaign has appeared on Transparency Corps"
     body :campaign => campaign, :user => user, :host => SITE_HOST
   end
 end
