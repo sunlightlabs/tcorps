f40257f0a1fa9edae77403914fef29425e1476fa
klondike <kprojection@gmail.com>
2009-07-10 14:10:36 -0400

    Updated from address to be a name

--- app/models/campaign_mailer.rb
+++ app/models/campaign_mailer.rb
@@ -2,7 +2,7 @@ class CampaignMailer < ActionMailer::Base
   
   def new_campaign(campaign, user)
     recipients user.email
-    from 'tcorps@sunlightfoundation.com'
+    from 'TransparencyCorps <tcorps@sunlightfoundation.com>'
     subject "A new campaign has appeared on TransparencyCorps"
     body :campaign => campaign, :user => user, :host => SITE_HOST
   end
