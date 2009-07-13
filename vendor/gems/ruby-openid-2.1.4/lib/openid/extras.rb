365841a884122614fabda72a826a32fe80494b4c
klondike <kprojection@gmail.com>
2009-07-13 16:00:46 -0400

    Added rack-bug into TCorps, with a slight monkeypatching of ruby-openid's monkeypatching of ActiveSupport's monkeypatching

--- vendor/gems/ruby-openid-2.1.4/lib/openid/extras.rb
+++ vendor/gems/ruby-openid-2.1.4/lib/openid/extras.rb
@@ -1,11 +1,11 @@
 class String
   def starts_with?(other)
-    head = self[0, other.length]
+    head = self[0, other.to_s.length]
     head == other
   end
 
   def ends_with?(other)
-    tail = self[-1 * other.length, other.length]
+    tail = self[-1 * other.to_s.length, other.to_s.length]
     tail == other
   end
 end
